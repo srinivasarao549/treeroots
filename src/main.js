1421f4139e215c5169b92d30fc77063919b476d0
hughfdjackson <hughfdjackson@googlemail.com>
2011-11-18 19:14:51 +0000

    manual test of the dependencies/inheritence between src/game and src/core/objectManager workin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 require(['entities', 'game'], function(entities, Game){
     
     
-    var canvas = document.getElementById("treeroots")
-    om.draw(canvas, canvas.getContext("2d"))
+    var canvas = document.getElementById("treeroots"),
+        game = new Game(),
+        cursor = new entities.Cursor()
+
+    game.add(cursor)
+
+    game.draw(canvas, canvas.getContext("2d"))
 
 })
