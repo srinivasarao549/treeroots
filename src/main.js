65e31158a82c0f026179c46e9182871fd7008786
hughfdjackson <hughfdjackson@googlemail.com>
2011-11-20 15:47:10 +0000

    clipping done/mouse handling for FF done

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-require(['entities', 'game'], function(entities, Game){
+require(['entities', 'game', 'lib/bean'], function(entities, Game, bean){
     
     var canvas = document.getElementById("treeroots"),
         game = new Game(canvas)
-
-    canvas.onmousemove = function(e){
-        game.input.mousex = e.offsetX;
-        game.input.mousey = e.offsetY;
-    }
+    
+    bean.add(canvas, 'mousemove', function(e){
+        game.input.mousex = e.clientX - canvas.offsetLeft;
+        game.input.mousey = e.clientY - canvas.offsetTop;
+    })
     
     game.add(new entities.Cursor)
     game.add(new entities.Ground)
-
 
    !function update(){
         game.update()
