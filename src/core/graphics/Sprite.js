d0dc262459ded7c40bfe9ed1e6e00f3f9757297c
hughfdjackson <hughfdjackson@googlemail.com>
2011-11-23 21:31:34 +0000

    i think i forgot to add something..

--- src/core/graphics/Sprite.js
+++ src/core/graphics/Sprite.js
@@ -6,8 +6,19 @@ define(["lib/compose"], function(compose){
                 x:0,
                 y:0,
                 
-                
             })
-        })
+        },
+        {
+            draw: function(camera, context, canvas){
+                var bound = this._bound || this
 
+                context.drawImage(this.image, bound.x - camera.x, bound.y - camera.y)
+                return this
+            },
+            bind: function(object){
+                this._bound = object
+                return this
+            }
+
+        })
 })
--- src/entities/cursor.js
+++ src/entities/cursor.js
@@ -3,10 +3,10 @@ define(["lib/compose", "core/graphics"], function(compose, g){
     return compose({
         init: function(game){
             this.input = game.input
-            this.sprite = new g.Sprite(game.images["cursor.png"])
+            this.sprite = new g.Sprite(game.images["cursor.png"]).bind(this)
         },
-        draw: function(offset, context, canvas){
-            context.drawImage(this.sprite.image, this.x, this.y)
+        draw: function(c, co, ca){
+            this.sprite.draw(c, co, ca)
         },
         update: function(td){
             this.x = this.input.mousex
