bd0225909fd9de2f76297b3e68590c7cc802f8b5
hughfdjackson <hughfdjackson@googlemail.com>
2011-11-23 21:59:28 +0000

    some really shoddy optimising to the core/graphics/sprite

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,4 +1,26 @@
-define(function(){
+define(["lib/compose", "core/graphics"], function(compose, g){
     
-    return function(){}
+    return compose(function(){
+            this.x = 0
+            this.y = 0
+        },
+        {
+            init: function(game){
+                this.input = game.input
+                this.sprite = new g.Sprite(game.images["seth.png"]).bind(this)
+            },
+            draw: function(c, co, ca){
+                this.sprite.draw(c, co, ca)
+            },
+            update: function(td){
+                var input = this.input,
+                    speed = 0.5 * td
+
+                if ( input.up ) this.y -= speed
+                if ( input.down ) this.y += speed
+
+                if ( input.left ) this.x -= speed
+                if ( input.right ) this.x += speed
+           }
+    })
 })
--- src/main.js
+++ src/main.js
@@ -9,6 +9,37 @@ require(['entities', 'game', 'level', 'lib/bean'], function(entities, Game, Leve
         game.input.mousey = e.clientY - canvas.offsetTop;
     })
     
+    bean.add(document, 'keydown', function(e){
+         var k = e.which,
+                input = game.input
+            
+            if ( k == 65 ) 
+                input.left = true
+            else if ( k == 68 ) 
+                input.right = true
+            else if ( k == 87 )
+                input.up = true
+            else if ( k == 83 )
+                input.down = true
+        
+    })
+
+    
+    bean.add(document, 'keyup', function(e){
+         var k = e.which,
+                input = game.input
+            
+            if ( k == 65 ) 
+                input.left = false
+            else if ( k == 68 ) 
+                input.right = false
+            else if ( k == 87 )
+                input.up = false
+            else if ( k == 83 )
+                input.down = false
+    })
+
+
     level.load({images: ["resources/images/seth.png", "resources/images/floor_1.png", "resources/images/cursor.png"],
                 objects: [
                      {type: "Player"}, {type: "Cursor"}, {type: "Ground"}
