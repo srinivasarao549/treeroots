53031f5cd040836590f99b6c38fde38cb610161d
hughfdjackson <hughfdjackson@googlemail.com>
2011-11-24 13:57:40 +0000

    refactoring game for readability

--- src/game.js
+++ src/game.js
@@ -8,44 +8,52 @@ define(["lib/compose", "core/objectManager"], function(compose, ObjectManager){
             this.canvas = canvas
             this.context = canvas.getContext("2d")       
         }
-
-        this.add = function(obj){
-            ObjectManager.prototype.add.call(this, obj)
-            if ( obj.init ) obj.init(this)
-        }
     
-        // stores we expect others to populate
-        this.input = {}
-        this.images = {}
     },
     {
-           update: function(td, input, canvas){
-                this.objects.forEach(function(val){
-                    if ( val.update ) val.update(td, input, canvas)
-                })
-                
-            },
-            
-           draw: function(){
-                var camera = this.camera,
-                    context = this.context,
-                    canvas = this.canvas
-                
-                if ( this.objects_modified ) {
-                    this.objects.sort(function(a, b){
-                    return a.z - b.z
-                    })
-                    this.objects_modified = false
-                }
-                
-                this.context.clearRect(0, 0, canvas.width, canvas.height)
-                this.objects.forEach(function(obj){
-                    if ( obj.draw ) obj.draw(camera, context, canvas)
-                })
-                
-            }
 
+        // Storage spaces
+        input: {},
+        images: {},
+
+        // EVENTS
+
+        //// Game.add
+        //
+        // Extends ObjectManager.add to call an object's init method
+        //
+        add: function(obj){
+            ObjectManager.prototype.add.call(this, obj)
+            if ( obj.init ) obj.init(this)
+        },
+
+        update: function(td, input, canvas){
+            this.objects.forEach(function(val){
+                if ( val.update ) val.update(td, input, canvas)
+            })
+
+        },
+
+        draw: function(){
+            var camera = this.camera,
+                context = this.context,
+                canvas = this.canvas
+
+            if ( this.objects_modified ) {
+                this.objects.sort(function(a, b){
+                    return a.z - b.z
+                })
+                
+                this.objects_modified = false
+              }
+
+            this.context.clearRect(0, 0, canvas.width, canvas.height)
+        
+            this.objects.forEach(function(obj){
+               if ( obj.draw ) obj.draw(camera, context, canvas)
+            })
         }
-     )
+
+    })
     
 })
